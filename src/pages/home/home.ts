0bebe3dbbbfd88d69c4b26122a89488a217f71d7
unknown <deriksadiki@gmail.com>
2019-08-12 21:52:13 +0200

    list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,13 +3,59 @@ import { NavController } from 'ionic-angular';
 import { DetailsPage } from '../details/details';
 import { SignInPage } from '../sign-in/sign-in';
 
+declare var firebase;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  tutors = new Array();
+  keys = new Array();
+
   constructor(public navCtrl: NavController) {
+  }
+//   saveApplication(id,trans,sub){
+//     return new Promise((accpt, rej) =>{
+//       var user = firebase.auth().currentUser;
+//       var date = new Date();
+//       var timestamp = date.getTime();
+//       firebase.database().ref("Applications/" + user.uid).set({
+//         Date: moment().format('l'),
+//         convo : timestamp,
+//         id : id,
+//         trans: trans,
+//         sub:sub,
+//     })
+//     this.checkVerificatiom();
+//    accpt('')
+// })
+//   }
+  storeTutors(tut,k){
+    this.tutors.push(tut);
+    this.keys.push(k)
+    console.log(this.tutors)
+  }
+
+  ionViewWillEnter(){
+    return new Promise((resolve, reject) => {
+        firebase.database().ref("users/").on("value", (data: any) => {
+          var details  =  data.val();
+          var keys = Object.keys(details);
+          for (var x = 0; x < keys.length; x++){
+            var k = keys[x];
+            firebase.database().ref("users/" + k).on("value", (data2: any) => {
+              var users = data2.val();
+              var keys2 = Object.keys(users);
+                if (users.role == "tutor" && users.status == "pending"){
+                    this.storeTutors(users,k)
+                }
+            })
+          }
+    })
+  })
+    
   }
 
   showDetails(){
